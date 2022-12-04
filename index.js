94ea92bfad4c087d65e74371020223cce2d4e9cd
singhzrepo <contactwithsid@gmail.com>
2022-12-04 13:30:39 +0530

    Added socket io server

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const passport = require('./config/passportJWT')
 const PORT = 8000
 const app = express()
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, { cors: {origin: "*"} });
+
+
 app.use(express.json())
 app.use(express.urlencoded())
 
 app.use(cors())
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('hello-server', (msg) => {
+        console.log(msg)
+    })
+
+    socket.emit('hello', 'hello client')
+  });
+
 passport.initialize()
 
 app.use('/', require('./routes'))
 
-app.listen(PORT, (err) => {
+server.listen(PORT, (err) => {
     if(err){
         console.log(err)
         return
